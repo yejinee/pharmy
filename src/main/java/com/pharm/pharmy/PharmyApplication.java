7e70615df2502431dbcf834fad608abed05f6085
Yejin <kimyj960911@gmail.com>
2023-03-23 18:37:20 +0900

    first setting

---
+++ src/main/java/com/pharm/pharmy/PharmyApplication.java
@@ -0,0 +1,13 @@
+package com.pharm.pharmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pharm/pharmy/PharmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharm.pharmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
